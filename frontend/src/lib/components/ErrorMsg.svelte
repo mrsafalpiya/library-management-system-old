85edbdddb615c93d9a5bb3541ba69bee3cda25bb
Safal Piya <mrsafalpiya@gmail.com>
2023-02-25 20:22:35 +0545

    Frontend: Error message: Added fadeOut animation on click

--- frontend/src/lib/components/ErrorMsg.svelte
+++ frontend/src/lib/components/ErrorMsg.svelte
@@ -3,6 +3,14 @@
   import Fa from "svelte-fa/src/fa.svelte";
 
   export let flash: any;
+
+  function handleOnClick(e: Event) {
+    const target = e.target as HTMLElement;
+    target.parentElement?.parentElement?.classList.add("removing");
+    setTimeout(() => {
+      $flash = undefined;
+    }, 100);
+  }
 </script>
 
 <div class="fixed inset-x-0 top-4 z-50 bg-transparent">
@@ -10,10 +18,26 @@
     <button
       class="btn flex gap-2 normal-case"
       class:btn-error={$flash.type == "error"}
-      on:click={() => ($flash = undefined)}
+      on:click={(e) => handleOnClick(e)}
     >
       <Fa class="inline-block" icon={faTriangleExclamation} />
       <p class="first-letter:capitalize">{$flash.message}</p>
     </button>
   </div>
 </div>
+
+<style>
+  :global(.removing) {
+    animation: fadeOut 0.1s;
+    animation-timing-function: ease-in-out;
+  }
+
+  @keyframes fadeOut {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
+</style>
