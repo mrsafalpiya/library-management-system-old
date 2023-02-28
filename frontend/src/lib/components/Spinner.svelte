8e608aad95f164d523da233de714f10d71c546f1
Safal Piya <mrsafalpiya@gmail.com>
2023-02-28 18:56:02 +0545

    Frontend: Added loading animation

---
+++ frontend/src/lib/components/Spinner.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  import Spinner from "$lib/images/spinner.svg";
+  import PrimeCollegeLogo from "$lib/images/prime-college.jpg";
+</script>
+
+<div class="fixed top-0 right-0 bottom-0 left-0 z-50 grid place-items-center bg-white" out:fade>
+  <div class="flex flex-col items-center">
+    <img src={PrimeCollegeLogo} alt="Prime College" class="w-full max-w-[250px]" />
+    <img src={Spinner} alt="Spinner" class="w-full max-w-[100px]" />
+  </div>
+</div>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,9 +3,20 @@
   import ErrorMsg from "$lib/components/ErrorMsg.svelte";
   import { initFlash } from "sveltekit-flash-message/client";
   import { page } from "$app/stores";
+  import { onMount } from "svelte";
+  import Spinner from "$lib/components/Spinner.svelte";
 
   const flash = initFlash(page);
+
+  let isLoading = true;
+
+  onMount(() => (isLoading = false));
 </script>
+
+<!-- Loading screen -->
+{#if isLoading}
+  <Spinner />
+{/if}
 
 <!-- Error Message -->
 {#if $flash}
